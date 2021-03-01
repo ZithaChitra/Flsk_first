45be282ec01ba28b1747ce9bb63d039c6249cf8a
zithachitra <realblessingchitra@gmail.com>
2021-03-01 14:33:51 +0000

    Account template update

--- Flask_Blog/flaskblog/forms.py
+++ Flask_Blog/flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -39,3 +40,30 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+	
+	submit = SubmitField('Update')
+
+	# def validate_field(self, field):
+	# 	if True:
+	# 		raise ValidationError("Validation Message")
+
+	def validate_username(self, username):
+		if username.data != current_user.user_name:
+			# checks if user already exists in the database
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError("That username is taken. Please use a different username.")
+
+	
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			# checks if user already exists in the database
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError("That email is taken. Please use a different email.")

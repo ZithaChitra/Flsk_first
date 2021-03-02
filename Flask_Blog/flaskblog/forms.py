75ba29a89c27567ae9d94ccf69086222f1ffff7d
zithachitra <realblessingchitra@gmail.com>
2021-03-02 04:39:00 +0000

    create post feature

--- Flask_Blog/flaskblog/forms.py
+++ Flask_Blog/flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -46,6 +47,7 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
 	email = StringField('Email',
                         validators=[DataRequired(), Email()])
+	picture = FileField("Update Profile Picture", validators=[FileAllowed(["jpg", "png"])])
 	
 	submit = SubmitField('Update')
 
@@ -54,7 +56,7 @@ class UpdateAccountForm(FlaskForm):
 	# 		raise ValidationError("Validation Message")
 
 	def validate_username(self, username):
-		if username.data != current_user.user_name:
+		if username.data != current_user.username:
 			# checks if user already exists in the database
 			user = User.query.filter_by(username=username.data).first()
 			if user:
@@ -67,3 +69,13 @@ class UpdateAccountForm(FlaskForm):
 			user = User.query.filter_by(email=email.data).first()
 			if user:
 				raise ValidationError("That email is taken. Please use a different email.")
+
+
+
+class PostForm(FlaskForm):
+	title = StringField("Title", validators=[DataRequired()])
+	content = TextAreaField("Content", validators=[DataRequired()])
+	submit = SubmitField("Post")
+
+
+
